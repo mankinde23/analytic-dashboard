d908e51b0bdb3b9017f7a89998a8e115a7474037
mankinde23 <damilolamakinde493@gmail.com>
2024-01-28 19:51:57 +0100

    learnt how to use grid to implement the layout for the application

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="grid grid-cols-2 grid-rows-2">
-      <div className="bg-red-800"> header</div>
-      <div className="bg-black">side bar </div>
-      <div className="bg-white "> main container </div>
+      <div className="bg-red-800 col-start-2 col-end-3  row-start-1 row-end-3">
+        {" "}
+        header
+      </div>
+      <div className="bg-black col-start-1 col-end-2 row-start-1 row-end-3 w-full">
+        side bar{" "}
+      </div>
+      <div className="bg-white col-start-2 col-end-3  row-start-2 row-end-3">
+        {" "}
+        main container{" "}
+      </div>
     </main>
   );
 }
